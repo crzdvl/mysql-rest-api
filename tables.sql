04b9dd50f696d2d3efad4ef8cf4d5021c93ded01
crzdvl <vik.coder@gmail.com>
2020-10-02 21:44:35 +0300

    fix: corrected sql file

--- tables.sql
+++ tables.sql
@@ -1,13 +1,14 @@
 CREATE TABLE `customers` (
   `id` int NOT NULL AUTO_INCREMENT,
   `id_location` varchar(45) DEFAULT NULL,
-  `username` varchar(50) DEFAULT NULL,
+  `firstname` varchar(50) DEFAULT NULL,
   `password` varchar(200) DEFAULT NULL,
   `email` varchar(200) DEFAULT NULL,
   `valid` tinyint DEFAULT '0',
+  `lastname` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
+) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+SELECT * FROM mydb.customers;
 
 CREATE TABLE `locations` (
   `id` int NOT NULL,
@@ -15,39 +16,34 @@ CREATE TABLE `locations` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-
 CREATE TABLE `products` (
-  `id` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `id_seller` varchar(45) DEFAULT NULL,
   `name` varchar(45) DEFAULT NULL,
   `seller` varchar(45) DEFAULT NULL,
-  `id_category` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `products_tags` (
-  `id_products` int DEFAULT NULL,
-  `id_tag` varchar(45) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  `id_product` bigint DEFAULT NULL,
+  `id_tag` bigint DEFAULT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-
-sellersCREATE TABLE `sellers` (
-  `id` int NOT NULL,
+CREATE TABLE `sellers` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `id_location` varchar(45) DEFAULT NULL,
-  `first name` varchar(45) DEFAULT NULL,
-  `last name` varchar(45) DEFAULT NULL,
+  `firstname` varchar(45) DEFAULT NULL,
+  `lastname` varchar(45) DEFAULT NULL,
   `email` varchar(45) DEFAULT NULL,
   `password` varchar(20) DEFAULT NULL,
   `valid` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `tags` (
-  `id` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
-
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
