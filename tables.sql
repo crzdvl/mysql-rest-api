141cac1b7b9b05f8c49a01a7a559fe60b2124132
crzdvl <vik.coder@gmail.com>
2020-10-05 21:17:20 +0300

    fix: fix mysql tables

--- tables.sql
+++ tables.sql
@@ -1,44 +1,42 @@
 CREATE TABLE `customers` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `id_location` varchar(45) DEFAULT NULL,
+  `id` bigint NOT NULL,
+  `location_id` bigint DEFAULT NULL,
   `firstname` varchar(50) DEFAULT NULL,
-  `password` varchar(200) DEFAULT NULL,
-  `email` varchar(200) DEFAULT NULL,
-  `valid` tinyint DEFAULT '0',
-  `lastname` varchar(45) DEFAULT NULL,
+  `lastname` varchar(50) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `password` varchar(16) DEFAULT NULL,
+  `verify` tinyint DEFAULT '0',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-SELECT * FROM mydb.customers;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `locations` (
-  `id` int NOT NULL,
-  `name` varchar(45) DEFAULT NULL,
+  `id` bigint NOT NULL,
+  `name` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `products` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `id_seller` varchar(45) DEFAULT NULL,
-  `name` varchar(45) DEFAULT NULL,
-  `seller` varchar(45) DEFAULT NULL,
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `seller_id` bigint DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `products_tags` (
-  `id_product` bigint DEFAULT NULL,
-  `id_tag` bigint DEFAULT NULL,
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `product_id` bigint DEFAULT NULL,
+  `tag_id` bigint DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 CREATE TABLE `sellers` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `id_location` varchar(45) DEFAULT NULL,
-  `firstname` varchar(45) DEFAULT NULL,
-  `lastname` varchar(45) DEFAULT NULL,
-  `email` varchar(45) DEFAULT NULL,
-  `password` varchar(20) DEFAULT NULL,
-  `valid` varchar(45) DEFAULT NULL,
+  `location_id` bigint DEFAULT NULL,
+  `firstname` varchar(50) DEFAULT NULL,
+  `lastname` varchar(50) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `password` varchar(16) DEFAULT NULL,
+  `verify` tinyint DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
@@ -47,3 +45,4 @@ CREATE TABLE `tags` (
   `name` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
